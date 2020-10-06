eebd02222d2074f0e2eb62cfdff1ded4789888d8
Mizeck Victor Tembo <temwanijr@gmail.com>
2020-10-06 14:50:16 +0200

    Second commit

--- biography/src/Pages/Components/Body/MainBody.js
+++ biography/src/Pages/Components/Body/MainBody.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Body =()=>{
     return(
         <div>
-            
+            <h2>Mizeck Victor Tembo</h2>
+            <p>My Names are Mizeck Victor Tembo. A Zambian, Christian, Software developer in Javascript. Mizeck Victor Tembo is currently a student at the University of Lusaka doing his bachelors degree in Information Systems And Technology</p>
         </div>
     );
 }
